6e767b962d55cc77229f162cac8cd44a3039ff4b
Illya Shershun <illyashershun08@gmail.com>
2024-03-11 19:12:02 +0100

    new hmw

--- goit-algo-hw-03_1.py
+++ goit-algo-hw-03_1.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 def get_days_from_today(date):
-    days_from_today="2020-10-09"
-    days_from_today=datetime.strptime(days_from_today,"%Y-%m-%d")
+    days_from_today=datetime.strptime(date,"%Y-%m-%d")
     today=datetime.now()
     difference=today-days_from_today
     return difference.days
-print(get_days_from_today('date'))
+print(get_days_from_today('2017-05-12'))
--- goit-algo-hw-03_2.py
+++ goit-algo-hw-03_2.py
@@ -1,5 +1,11 @@
 import random
-def get_numbers_ticket(min,max,quantite):
-    return {"min":min,"max":max,"quantite":quantite}
-l = list(range(1, 36))
-print(random.sample(l, k=5))
+
+def get_numbers_ticket(min,max,quantity):
+    if min < 1 or max > 1000 or min >= max or quantity < 1 or quantity > (max - min + 1):
+        return []
+    numbers_set = set()
+    while len(numbers_set) < quantity:
+        numbers_set.add(random.randint(min, max))
+    num_list = sorted(list(numbers_set))
+    return num_list
+print(get_numbers_ticket(1, 36, 5))
